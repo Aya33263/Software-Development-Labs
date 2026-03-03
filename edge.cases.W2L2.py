f7158f0a522cd621e2c299155361a66ac876586f
Aya33263 <ayaalzwghaibi@gmail.com>
2026-03-03 13:54:18 +1300

    Add files via upload

---
+++ edge.cases.W2L2.py
@@ -0,0 +1,14 @@
+score =int(input("enter the number : "))
+
+if score >100:
+    print("invalid")
+elif score>=90:
+    print ("Grade: A")
+elif score>=80 :
+    print("Grade :B ")
+elif score>=70 :
+    print("Grade :c ")
+elif score >=0 :
+    print("Grade :F ")
+else:
+    print("invalid ")
